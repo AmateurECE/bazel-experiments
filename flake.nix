4c65f48832a3edcae024e8af6b51899de27e0155
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-02-11 14:54:57 -0600

    Introduce a reproducible conan configuration with metalwork

--- flake.nix
+++ flake.nix
@@ -3,16 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    metalwork = {
+      type = "github";
+      owner = "AmateurECE";
+      repo = "metalwork";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: let
+  outputs = { self, nixpkgs, metalwork, ... }: let
     forAllSystems = nixpkgs.lib.genAttrs [
       "x86_64-linux"
       "aarch64-linux"
     ];
   in {
     devShells = forAllSystems(system:
-      let pkgs = import nixpkgs {
+      let
+        pkgs = import nixpkgs {
           inherit system;
           crossSystem = {
             config = "arm-none-eabi";
@@ -23,16 +29,24 @@
             };
           };
         };
+        conan-config = metalwork.lib.mkConanConfig pkgs;
       in {
         default = pkgs.callPackage(
           {}: pkgs.mkShell ({
-            nativeBuildInputs = with pkgs.pkgsBuildHost; [ bazel ];
+            nativeBuildInputs = with pkgs.pkgsBuildHost; [
+              bazel
+              conan-config
+            ];
             buildInputs = with pkgs.pkgsBuildTarget; [ gcc ];
 
             # Needed to allow ld-linux.so to locate shared libraries installed
             # by nix packages linked to by executables that are compiled for
             # the host
             LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
+            shellHook = ''
+              conan config install ${conan-config}/conan
+            '';
           })
         ) {};
       }
