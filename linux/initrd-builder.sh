e4e9e15e429b4cb543b35fada53598bd22e0f569
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-02-22 19:54:34 -0600

    Create a task that populates an initrd cpio archive
    This task takes one ELF file, patches it to fix the library runpath and
    interpreter, and copies any dynamically linked dependencies into a
    single CPIO archive. The idea is that we can have Kbuild bundle this up
    with the kernel to obtain an initramfs-only image.

---
+++ linux/initrd-builder.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+while getopts ":o:f:" flag; do
+  case "$flag" in
+    o) OUTPUT_DIRECTORY=$OPTARG;;
+    f) FILE_NAME=$OPTARG;;
+  esac
+done
+
+INPUT=${@:$OPTIND}
+
+# Ensure the output directory exists.
+mkdir -p $OUTPUT_DIRECTORY
+
+# Copy input executable to output directory.
+init=$OUTPUT_DIRECTORY/init
+install -m755 $INPUT $init
+
+# Grab shared libraries from the toolchain
+IFS=$'\n'; shared_libraries=($(readelf -d $INPUT | grep 'Shared library' | sed 's/^[^[]*\[\([^]]\+\)\].*$/\1/'))
+
+# Fix the runpath
+IFS=':'; runpath=($(readelf -d $INPUT | grep 'Library runpath' | sed 's/^[^[]*\[\([^]]\+\)\].*$/\1/'))
+
+unset IFS
+
+libdir=$OUTPUT_DIRECTORY/lib
+mkdir -p $OUTPUT_DIRECTORY/lib
+for shared_library in "${shared_libraries[@]}"; do
+  for directory in "${runpath[@]}"; do
+    file=$directory/$shared_library
+    if [ -f $file ]; then
+      install -m755 $file $libdir
+    fi
+  done
+done
+
+patchelf --set-rpath /lib $init
+
+# Fix the interpreter
+interpreter=$(readelf -p .interp $INPUT | awk '/\.so/{print $3}')
+
+install -m755 $interpreter $libdir
+
+patchelf --set-interpreter /lib/$(basename $interpreter) $init
+
+# Stuff into a cpio archive
+(cd $OUTPUT_DIRECTORY && find . ! -path . ! -path ./archive-contents.txt ! -path ./$FILE_NAME) \
+  >$OUTPUT_DIRECTORY/archive-contents.txt
+(cd $OUTPUT_DIRECTORY && cpio -o -H newc -R 0:0 | gzip -c) \
+  <$OUTPUT_DIRECTORY/archive-contents.txt >$FILE_NAME
--- linux/kernel.bzl
+++ linux/kernel.bzl
@@ -110,3 +110,42 @@ kernel_headers = rule(
   },
   toolchains = ["@rules_cc//cc:toolchain_type"],
 )
+
+
+def _single_app_cpio_initrd_impl(ctx):
+  """Create a cpio archive with a single application (init) to run under the
+  kernel. Automatically populates libc in the archive, if the application
+  links to one, but currently cannot automatically populate other kinds of
+  dependencies."""
+  archive = ctx.actions.declare_file("initrd.cpio.gz")
+
+  args = ctx.actions.args()
+  args.add("-o", "builddir")
+  args.add("-f", archive.path)
+  args.add_all(ctx.files.init)
+
+  ctx.actions.run(
+    mnemonic = "WriteInitrd",
+    progress_message = 'Writing %s'.format(archive.path),
+    executable = ctx.executable._builder.path,
+    arguments = [args],
+    outputs = [archive],
+    inputs = ctx.files.init + [ctx.executable._builder],
+    use_default_shell_env = True,
+  )
+
+  return [DefaultInfo(files = depset([archive]))]
+
+
+single_app_cpio_initrd = rule(
+  implementation = _single_app_cpio_initrd_impl,
+  attrs = {
+    'init': attr.label(),
+    '_builder': attr.label(
+      default = Label(":initrd-builder.sh"),
+      cfg = "exec",
+      executable = True,
+      allow_files = True,
+    ),
+  },
+)
