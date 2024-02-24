6a328c0661325b216ec71c585f63e25fa6ad1aa3
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-02-24 17:36:21 -0600

    Add /dev/console to the CPIO archive
    This results in another point of non-hermeticity for this rule. In the
    future, I should write an application that generates the CPIO archive so
    that nodes can be added at will.

--- linux/initrd-builder.sh
+++ linux/initrd-builder.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 while getopts ":o:f:" flag; do
   case "$flag" in
@@ -18,14 +19,15 @@ mkdir -p $OUTPUT_DIRECTORY
 init=$OUTPUT_DIRECTORY/init
 install -m755 $INPUT $init
 
-# Grab shared libraries from the toolchain
+# Extract linked shared libraries
 IFS=$'\n'; shared_libraries=($(readelf -d $INPUT | grep 'Shared library' | sed 's/^[^[]*\[\([^]]\+\)\].*$/\1/'))
 
-# Fix the runpath
+# Extract the library runpath
 IFS=':'; runpath=($(readelf -d $INPUT | grep 'Library runpath' | sed 's/^[^[]*\[\([^]]\+\)\].*$/\1/'))
 
 unset IFS
 
+# Install shared libraries from the toolchain into the root filesystem
 libdir=$OUTPUT_DIRECTORY/lib
 mkdir -p $OUTPUT_DIRECTORY/lib
 for shared_library in "${shared_libraries[@]}"; do
@@ -37,17 +39,25 @@ for shared_library in "${shared_libraries[@]}"; do
   done
 done
 
+# Fix library runpath
 patchelf --set-rpath /lib $init
 
 # Fix the interpreter
 interpreter=$(readelf -p .interp $INPUT | awk '/\.so/{print $3}')
-
 install -m755 $interpreter $libdir
-
 patchelf --set-interpreter /lib/$(basename $interpreter) $init
+
+# Initialize the /dev directory
+install -d -m755 $OUTPUT_DIRECTORY/dev
 
 # Stuff into a cpio archive
 (cd $OUTPUT_DIRECTORY && find . ! -path . ! -path ./archive-contents.txt ! -path ./$FILE_NAME) \
   >$OUTPUT_DIRECTORY/archive-contents.txt
-(cd $OUTPUT_DIRECTORY && cpio -o -H newc -R 0:0) \
+
+# Copy /dev/console into the archive.
+# TODO: This is non-hermetic
+printf '/dev/console\n' >> $OUTPUT_DIRECTORY/archive-contents.txt
+
+(cd $OUTPUT_DIRECTORY && \
+  cpio -o --reproducible --no-absolute-filenames -H newc -R 0:0) \
   <$OUTPUT_DIRECTORY/archive-contents.txt >$FILE_NAME
